92eb2d145ebe20b6a2d7389c6af57f43d85b2e3b
Burger-boi-bozo <burgerboibozo@gmail.com>
2026-09-13 18:43:26 -0500

    Run the v4 application assembly

--- app/serve.py
+++ app/serve.py
@@ -4,7 +4,7 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app.runtime:app",
+        "app.runtime_v4:app",
         host="0.0.0.0",
         port=int(os.getenv("PORT", "8080")),
         workers=1,
